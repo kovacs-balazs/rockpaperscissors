c92ba319628702c4902cbbab4279df2a28f24791
Balázs Kovács <56174644+kovacs-balazs@users.noreply.github.com>
2024-01-18 15:33:04 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -1,13 +1,6 @@
-import webbrowser
 import random
 
 weight = {"kő": "olló", "papír": "kő", "olló": "papír"}
-troll_mode: bool = False
-
-
-def set_troll(troll: bool):
-    global troll_mode
-    troll_mode = troll
 
 
 def start_game(selected: str):
@@ -20,9 +13,3 @@ def start_game(selected: str):
         print("Vesztettél.")
     else:
         print("Nyertél.")
-
-    if troll_mode:
-        url = "https://youtu.be/wh9QLjk3M2k?si=m_vE0uJrvbtZ41mX"
-
-        for i in range(100):
-            webbrowser.open(url, new=0, autoraise=True)
